bb0e31e7e4688e08a53de7692f2bd47ffed947be
Hammadkahloon <ha4827603@gmail.com>
2024-08-31 12:21:04 +0500

    variable and operators in python3

---
+++ operators/bitwise.py
@@ -0,0 +1,14 @@
+x=10
+y=8
+print(bin(x))
+print(bin(y))
+print(bin(x&y),x&y)  #1010
+print(bin(x|y),x|y)  #1000
+print(bin(x^y),x^y)  #0010
+
+# 0b1010  8
+# 0b1000  10
+
+#& 1010  8
+#| 1000 10
+#^ 0010  2
